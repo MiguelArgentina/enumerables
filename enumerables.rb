9d568dcfb0851e5c90420994e8a0b3df7195fd23
Camilo Patino <jcamilop96@gmail.com>
2021-02-11 19:15:04 -0500

    add my_all and my_none methods

--- enumerables.rb
+++ enumerables.rb
@@ -29,6 +29,32 @@ module Enumerable
       ary_aux = []
       my_each { |item| ary_aux << item if yield item }
       ary_aux
+  end
+
+  def my_all(isEmpty = nil)
+    if block.given?
+      to_a.my_each { |item| if yield(item)? : return true : return false }
+    else
+      case isEmpty
+      when nil
+        to_a.my_each { |item| return false if item == false || item.nil? }
+      when Class
+        to_a.my_each { |item| if [item.class, item.class.superclass].include?(isEmpty)? : return true : return false }
+      when Regexp
+        to_a.my_each { |item| return false unless isEmpty.match(item) }
+      else
+        to_a.my_each { |item| if item == isEmpty? : return true : return false }
+      end
     end
+    true
+  end
+
+  def my_none(isEmpty = nil)
+    if block.given?
+      !my_any(&Proc.new)
+    else
+      !my_any(isEmpty)
+    end
+  end
 
 end
