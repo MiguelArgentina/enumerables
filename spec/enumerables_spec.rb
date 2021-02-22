bc33860c58af89db763d0ba73e6610ae2ab9ff6b
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-02-22 13:53:31 -0300

    Add rspec for my_select and my_all? methods

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -6,6 +6,7 @@ describe Enumerable do
   let(:ary) {%w[a b c]}
   let(:rng) {(1..5)}
   let(:block){|item| print item, "--"}
+  let(:sym_ary) {[:foo, :bar, :sym]}
   describe "#my_each" do
     it "Return an enumerator if no block is given" do
       expect(ary.my_each).to be_an(Enumerator)
@@ -30,8 +31,46 @@ describe Enumerable do
       end
 
       it "returns the array after calling the given block once for each slement in self::range" do
-        expect(rng.my_each_with_index{:block}).to be(rng.eachwith_index{:block})
+        expect(rng.my_each_with_index{:block}).to be(rng.each_with_index{:block})
       end
     end
   end
+
+  describe "#my_select" do
+    let(:block){|item| item > 3}
+    let(:sym_block){|item| item == :foo}
+    it 'returns an Enumerator if no block is given' do
+      expect(rng.my_select).to be_an(Enumerator)
+    end
+
+    context "when a block is given" do
+      it 'returns an array with all elements for which the given block returns a true value.' do
+        expect(rng.my_select{:block}).to eq(rng.select{:block})
+      end
+
+      it 'returns an array with all elements for which the given block returns a true value.' do
+        expect(sym_ary.my_select{:sym_block}).to eq(sym_ary.select{:sym_block})
+      end
+    end
+  end
+
+  describe "#my_all?" do
+    let(:block){|item| item > 3}
+    let(:block_is_numeric){|item| item > 0}
+
+    context "when a block is given" do
+      it 'The method returns true if the block never returns false or nil after applying the block to every item' do
+        expect(rng.my_all?{:block}).to eq(rng.all?{:block})
+      end
+
+      it 'The method returns true if the block never returns false or nil after applying the block to every item' do
+        expect(rng.my_all?{:block_is_numeric}).to eq(rng.all?{:blblock_is_numericock})
+      end
+
+      it 'The method returns true if the block never returns false or nil after applying the block to every item' do
+        expect(rng.my_all?(Numeric)).to eq(rng.all?(Numeric))
+      end
+    end
+  end
+
 end
