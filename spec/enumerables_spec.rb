cfadbd502effd68b2a95241f5458d3fa02a2272a
George <ubong_george@ymail.com>
2021-02-22 17:20:08 +0100

    Add test case for my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -17,4 +17,21 @@ describe Enumerable do
       expect(rng.my_each{:block}).to be(rng.each{:block})
     end
   end
+
+  describe "#my_each_with_index" do
+    let(:block){|item, index| print "Item: #{item}, Index: #{index}"}
+    it 'calls block with two arguments, the item and its index, for each item in enum' do
+      expect(ary.my_each_with_index).to be_an(Enumerator)
+    end
+
+    context "when a block is given" do
+      it 'returns the array after calling the given block once for each slement in self' do
+        expect(ary.my_each_with_index{:block}).to be(ary.each_with_index{:block})
+      end
+
+      it "returns the array after calling the given block once for each slement in self::range" do
+        expect(rng.my_each_with_index{:block}).to be(rng.eachwith_index{:block})
+      end
+    end
+  end
 end
